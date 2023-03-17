0d429c72ddc2a931604a0ba4e4f95310b9fbb564
Sandeep Devendra <83231651+SAN-R-D@users.noreply.github.com>
2023-03-17 17:48:12 +0530

    Update TB-AWS-SS-PRD.sh

--- environment/TB-AWS-SS-PRD.sh
+++ environment/TB-AWS-SS-PRD.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
+export AWS_ACCESS_KEY_ID=AKIA273R4Y72PBMP6SHU
+export AWS_SECRET_ACCESS_KEY=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
+export AWS_DEFAULT_REGION="ap-south-1"
 export INSTANCE_TYPE='t2.micro'
-export SG_IN='sg-005f02499d405d82c'
+export SG_IN='sg-0c986175bf8c06480'
 export SG_OUT='sg-0750c07b9befac83f'
-export SUBNET='subnet-34827f52'
+export SUBNET='subnet-0040a8745ed40eb16'
