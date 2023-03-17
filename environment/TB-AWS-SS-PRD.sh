eee86625937e42b176c07c8aaada187fb5db4e1c
Sandeep Devendra <83231651+SAN-R-D@users.noreply.github.com>
2023-03-17 16:57:20 +0530

    Update TB-AWS-SS-PRD.sh

--- environment/TB-AWS-SS-PRD.sh
+++ environment/TB-AWS-SS-PRD.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export INSTANCE_TYPE='m5.2xlarge'
+export INSTANCE_TYPE='t2.micro'
 export SG_IN='sg-005f02499d405d82c'
 export SG_OUT='sg-0750c07b9befac83f'
 export SUBNET='subnet-34827f52'
